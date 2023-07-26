639b1f6d37495952870b8d24ac4216f761fb835e
Agus Sugandi <agus.sugandi95@gmail.com>
2023-07-26 14:10:51 +0700

    fix problem

--- app/dashboard/lib/getdata.js
+++ app/dashboard/lib/getdata.js
@@ -1,5 +1,9 @@
 export async function getData(url){
-  const res = await fetch(`http://${process.env.NEXT_PUBLIC_MYSQL_HOST}${url}`, { cache : "no-store" })
-  return res.json();
+  try{
+    const res = await fetch(`http://${process.env.NEXT_PUBLIC_MYSQL_HOST}${url}`, { cache : "no-store" })
+    return res.json();
+  }catch{
+    return res = [{}]
+  }
 }
 
