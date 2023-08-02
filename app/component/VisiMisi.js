2b9ca8667f12b66e9ec6ff6109de887f3c53ae7f
Agus Sugandi <agus.sugandi95@gmail.com>
2023-08-02 14:19:53 +0700

    visi misi

--- app/component/VisiMisi.js
+++ app/component/VisiMisi.js
@@ -3,20 +3,20 @@ import visi from "../../public/visi.png"
 
 export default function VisiMisi(){
     return(
-        <section className="py-10 bg-white sm:py-16 lg:py-24">
+        <section className="py-10 bg-gradient-to-t from-blue-primary to-blue-700 sm:py-16 lg:py-24">
             <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
                 <div className="grid items-center grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-24">
                     <div className="">
                         <div className="mb-10">
-                            <h2 className="text-3xl font-bold leading-tight text-blue-primary sm:text-4xl lg:text-5xl">Visi</h2>
-                            <p className="mt-6 text-base text-gray-600">Visi kami adalah menjadi penyedia jasa pengurusan transportasi terkemuka yang
+                            <h2 className="text-3xl font-bold leading-tight white sm:text-4xl lg:text-5xl">Visi</h2>
+                            <p className="mt-6 text-base text-white/70">Visi kami adalah menjadi penyedia jasa pengurusan transportasi terkemuka yang
                             diandalkan oleh pelanggan kami. Kami bertujuan untuk meningkatkan konektivitas dan
                             efisiensi dalam industri transportasi </p>
                         </div>
 
                         <div>
-                            <h2 className="text-3xl font-bold leading-tight text-blue-primary sm:text-4xl lg:text-5xl">Misi</h2>
-                            <ol className="mt-6 text-base text-gray-600 list-decimal">
+                            <h2 className="text-3xl font-bold leading-tight white sm:text-4xl lg:text-5xl">Misi</h2>
+                            <ol className="mt-6 text-base text-white/70 list-decimal">
                                 <li className="ml-3">Menyediakan solusi transportasi yang aman, nyaman, dan efisien kepada pelanggan kami.</li>
                                 <li className="ml-3">Mengoptimalkan penggunaan teknologi untuk meningkatkan pengalaman pelanggan dan efisiensi operasional.</li>
                                 <li className="ml-3">Menjalin kemitraan yang kuat dengan mitra transportasi dan pemasok untuk memberikan layanan yang terbaik.</li>
