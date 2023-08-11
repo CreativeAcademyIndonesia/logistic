e671c4703b0146e45dab40992c26605c24108d95
Agus Sugandi <agus.sugandi95@gmail.com>
2023-08-11 20:52:36 +0700

    middle ware

--- middleware.js
+++ middleware.js
@@ -1,21 +1,22 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export function middleware() {
-    // retrieve the current response
-    const res = NextResponse.next()
+// export function middleware() {
+//     // retrieve the current response
+//     const res = NextResponse.next()
 
-    // add the CORS headers to the response
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', 'http://hasfiprimalogistik.com') // replace this your actual origin
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
+//     // add the CORS headers to the response
+//     res.headers.append('Access-Control-Allow-Credentials', "true")
+//     res.headers.append('Access-Control-Allow-Origin', 'http://hasfiprimalogistik.com') // replace this your actual origin
+//     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+//     res.headers.append(
+//         'Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     )
 
-    return res
-}
+//     return res
+// }
 
 export {default} from "next-auth/middleware"
 
-export const config = { matcher : ["/dashboard/:path*", "/api/:path*"] }
+export const config = { matcher : ["/dashboard/:path*"] }
+// export const config = { matcher : ["/dashboard/:path*", "/api/:path*"] }
